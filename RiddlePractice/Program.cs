f70f8c3cb4fc6ef2f1d6a31d9165beec8a6beff5
joshuatreeee <joshuatreeee@gmail.com>
2020-01-16 11:36:15 -0500

    got an acceptable loop to work

---
+++ RiddlePractice/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RiddlePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string name = (Console.ReadLine().ToLower());
+            Console.WriteLine("What is your last name?");
+            string lastName = (Console.ReadLine().ToLower());
+            Console.WriteLine("Your name is "+name+" "+lastName+".");
+            int nameLength = name.Length;
+            int lastNameLength = lastName.Length;
+            if (nameLength > lastNameLength)
+            {
+                Console.WriteLine("Your first name is longer.");
+            }
+            else if (lastNameLength > nameLength)
+            {
+                Console.WriteLine("Your last name is longer.");
+            }
+            else
+            {
+                Console.WriteLine("Your first and last name are the same length.");
+            }
+            Console.WriteLine("What is long, brown, and sticky?");
+            string answer;
+            do
+            {
+                answer = (Console.ReadLine().ToLower());
+                if (answer == "stick")
+                {
+                    Console.WriteLine("That is correct!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please try again");
+                }
+            }
+            while (answer != "stick");
+        }
+    }
+}
